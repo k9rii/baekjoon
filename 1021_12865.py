72434dcf42c5687a264de43b1c4528819eab1266
김규리 <cherrychel@naver.com>
2025-10-21 15:30:49 +0900

    평범한 배낭

---
+++ 1021_12865.py
@@ -0,0 +1,22 @@
+'''
+n개의 물건
+w 무게
+v 가치
+최대 k만큼의 무게를 넣을 수 있는 배낭
+'''
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+bag = [list(map(int, input().split())) for _ in range(n)] #(무게, 가치)
+dp = [[0]*(k+1) for _ in range(n+1)] #가치 저장
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if j >= bag[i-1][0]:
+            dp[i][j] = max(bag[i-1][1]+dp[i-1][j-bag[i-1][0]], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[n][k])
+
